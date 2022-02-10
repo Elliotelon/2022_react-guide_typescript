86ac6edd10ecb549f8f0019eb40473069aa39196
kimminkyu <kmk50009@naver.com>
2022-02-11 02:46:32 +0900

    Add removeTodoHandler

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -12,10 +12,17 @@ function App() {
       return prevTodos.concat(newTodo);
     });
   };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
+  }
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
